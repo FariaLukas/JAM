8e66b00d300c4903e45165c4b29dc89a3d709860
Lucas Faria <50430652+FariaMASTER@users.noreply.github.com>
2021-09-26 16:54:13 -0300

    FIx animations

--- Assets/Scripts/Enemies/EnemyRanged.cs
+++ Assets/Scripts/Enemies/EnemyRanged.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class EnemyRanged : EnemyBase
 {
     private Weapon weapon;
-
+    private bool _canAttack;
+    private Renderer _render;
     protected override void Init()
     {
         base.Init();
         weapon = GetComponent<Weapon>();
         weapon.OverrideRatio(data.delay);
+        if (!_render)
+            _render = GetComponent<SpriteRenderer>();
     }
 
     public override void Attack()
     {
         if (dead) return;
+        if (!_canAttack) return;
+
         if (player)
             weapon.Shoot(player.transform, data.damage, animator, data.attackTrigger);
+    }
+
+    private void OnBecameVisible()
+    {
+        Invoke(nameof(Activate), 1f);
+        print("A");
+    }
+
+    private void Activate()
+    {
+        if (_render.isVisible)
+            _canAttack = true;
+
+        print("B");
+    }
+
+    private void OnBecameInvisible()
+    {
+        _canAttack = false;
     }
 }
